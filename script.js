99f4663140085e399be3e3683234b57c03c95609
faustorcabrera <fcabrera96@icloud.com>
2023-08-02 09:10:13 -0500

    Added two questions

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// An array of questions. Each questio is an objec
+// An array of questions. Each question is an object with a 'question' property and an 'answer' property.
+const questions = [
+    { question: 'What is is 2+2?',  answer: '4' 
+    }
+    { question: 'What is the capital of France?', answer: 'Paris'
+    }
+    // I am only adding two questions for the moment. Really just getting used to how to write JS.
+];
